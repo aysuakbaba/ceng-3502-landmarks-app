73453ca19769e792b6d95ad5350dc7b865dece97
aysuuakbaba <aysuakbaba@posta.mu.edu.tr>
2025-04-26 19:32:52 +0300

    add cors to server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -17,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to my custom Node.js backend!");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
